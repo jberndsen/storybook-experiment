c10867202d983dc81611c5639eabf8e3613cbb23
jeroen <jeroen.berndsen@kramp.com>
2019-08-12 14:16:16 +0200

    initial component and itcss setup

---
+++ my-guide/src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './HelloWorld.scss';
+
+const HelloWorld: React.FC = () => (
+  <div className="ui-hello-world">Hello, world!</div>
+);
+
+export default HelloWorld;
