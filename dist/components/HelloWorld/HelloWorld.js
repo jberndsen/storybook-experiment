53777546a063d35db624663e50fcb3b26b283133
jeroen <jeroen.berndsen@kramp.com>
2019-08-15 13:44:22 +0200

    added ts build and style copy to dist

---
+++ dist/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './HelloWorld.scss';
+const HelloWorld = () => (React.createElement("div", { className: "ui-hello-world" }, "Hello, world!"));
+export default HelloWorld;
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+import HelloWorld from './components/HelloWorld/HelloWorld';
+export { HelloWorld };
